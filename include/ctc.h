c0b90eff1a71456adcba86b81342445954fe3882
root <root@yq01-qianmo-com-255-129-13.yq01.baidu.com>
2021-03-23 08:14:37 +0000

    fix cuda api names to hip api names

--- include/ctc.h
+++ include/ctc.h
@@ -20,6 +20,14 @@
 extern "C" {
 #endif
 
+#if (defined(__CUDACC__) || defined(__HIPCC__))
+#ifdef __HIPCC__
+#include <hip/hip_runtime.h>
+#else
+#include <cuda_runtime.h>
+#endif
+#endif
+
 //forward declare of CUDA typedef to avoid needing to pull in CUDA headers
 #ifdef __CUDACC__
 typedef struct CUstream_st* CUstream;
